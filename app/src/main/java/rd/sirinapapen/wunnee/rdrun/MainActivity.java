c3bd55e1313f9707cd282e93fc03b7cb1201efde
test <tes@abc.cmt>
2016-08-29 13:06:27 +0700

    test

---
+++ app/src/main/java/rd/sirinapapen/wunnee/rdrun/MainActivity.java
@@ -0,0 +1,14 @@
+package rd.sirinapapen.wunnee.rdrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
