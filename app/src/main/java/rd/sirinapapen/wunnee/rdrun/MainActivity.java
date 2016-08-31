284cd0d1c35bb5982714bb362f9245ec5ee46a55
test <tes@abc.cmt>
2016-08-31 15:09:11 +0700

    Read json finish

--- app/src/main/java/rd/sirinapapen/wunnee/rdrun/MainActivity.java
+++ app/src/main/java/rd/sirinapapen/wunnee/rdrun/MainActivity.java
@@ -1,12 +1,19 @@
 package rd.sirinapapen.wunnee.rdrun;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +40,48 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method นี่คือ เมธอด
 
+    //Create Inner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+        //Explicit
+        private Context context;
+        private String myUserString, myPasswordString;
+        private static final String urlJSON = "http://swiftcodingthai.com/rd/get_user_master.php";
+
+        public SynUser(Context context, String myUserString, String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+           this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("31AugV2", "e doInBack ==> " + e.toString());
+                return null;
+            }
+
+         }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("31AugV2", "JSon ==> " + s);
+
+        } // onPost
+
+
+    }   //SynUser Class
 
     public void clickSignInMain(View view) {
 
@@ -47,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
                     "Please Fill All Every Blank");
 
         } else {
+            //No Space
+            SynUser synUser = new SynUser(this, userString, passwordString);
+            synUser.execute();
+
         }
 
 
