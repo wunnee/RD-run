7fd4c2eca1dd548be567246807ea0508075236e9
test <tes@abc.cmt>
2016-09-01 11:48:03 +0700

    Design UI Service

--- app/src/main/java/rd/sirinapapen/wunnee/rdrun/ServiceActivity.java
+++ app/src/main/java/rd/sirinapapen/wunnee/rdrun/ServiceActivity.java
@@ -17,7 +17,7 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service);
+        setContentView(R.layout.my_service);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
