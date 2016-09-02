d4a213e6de281070d8b0c9a486471ddb413ce2e8
test <tes@abc.cmt>
2016-09-02 14:07:41 +0700

    Show Title

--- app/src/main/java/rd/sirinapapen/wunnee/rdrun/ServiceActivity.java
+++ app/src/main/java/rd/sirinapapen/wunnee/rdrun/ServiceActivity.java
@@ -17,7 +17,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
@@ -25,6 +27,9 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -87,7 +92,9 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         private Context context;
         private GoogleMap googleMap;
         private static final String urljson = "http://swiftcodingthai.com/rd/get_user_master.php";
-
+        private String[]nameStrings,surnameStrings;
+        private int[] avataInts;
+        private double[] latDoubles, lngDoubles;
 
 
         public SynAllUser(Context context, GoogleMap googleMap) {
@@ -119,8 +126,45 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             Log.d("2SepV2", "JSON ==> " + s);
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                nameStrings = new String[jsonArray.length()];
+                surnameStrings = new String[jsonArray.length()];
+                avataInts = new int[jsonArray.length()];
+                latDoubles = new double[jsonArray.length()];
+                lngDoubles = new double[jsonArray.length()];
+                //จองหน่วยความจำ
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    nameStrings[i] = jsonObject.getString("Name");
+                    surnameStrings[i] = jsonObject.getString("Surname");
+                    avataInts[i] = Integer.parseInt(jsonObject.getString("Avata"));
+                    latDoubles[i] = Double.parseDouble(jsonObject.getString("Lat"));
+                    lngDoubles[i] = Double.parseDouble(jsonObject.getString("Lng"));
+
+                    // Create Market คือ icon ที่อยู่บนแผนที่
+
+                    MyConstant myConstant = new MyConstant();
+                    int[] iconInts = myConstant.getAvataInts();
 
 
+                    googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(latDoubles[i], lngDoubles[i]))
+                    .icon(BitmapDescriptorFactory.fromResource(iconInts[avataInts[i]]))
+                    .title(nameStrings[i] + " " + surnameStrings[i]));
+
+                    Log.d("2SepV3", "Name(" + i + ") = " + nameStrings[i]);
+                    Log.d("2SepV3", "Lat(" + i + ") = " + latDoubles[i]);
+                    Log.d("2SepV3", "Lng(" + i + ") = " + lngDoubles[i]);
+                    Log.d("2SepV3", "=================================");
+
+                }// for
+
+            } catch (Exception e) {
+                Log.d("2SepV3", "e onPost ==> " + e.toString());
+            }
 
 
         } //onPost
@@ -235,6 +279,9 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     }   // myLoop
 
     private void createMarker() {
+
+        //Clear Market
+        mMap.clear();
 
         SynAllUser synAllUser = new SynAllUser(this, mMap);
         synAllUser.execute();
